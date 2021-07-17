b2e7848c5a8001f562c81990f1d95a8a38032f1d
Renato Pereira Santos <77284595+renatodev95@users.noreply.github.com>
2021-07-17 16:51:50 -0300

    Update

--- exercicios/src/cpf/Usuario.java
+++ exercicios/src/cpf/Usuario.java
@@ -4,7 +4,7 @@ public class Usuario {
 
 	public static void main(String[] args) {
 
-		Cpf p1 = new Cpf("Renato", "049.672.941-11");
+		Cpf p1 = new Cpf("Renato", "049.672.941-89");
 
 		System.out.println(p1.gerarDigito("049672941"));
 	}
